1a2352b5805810090119e385c7a9ab5603229729
lmmartin28 <95064355+lmmartin28@users.noreply.github.com>
2024-10-14 16:06:56 -0400

    Update and rename EDOF_WFOV_figure_20230107-DesignScriptClean.py to EDOF_WFOV_bilayer_PSF_simulation.py

--- EDOF_WFOV_bilayer_PSF_simulation.py
+++ EDOF_WFOV_bilayer_PSF_simulation.py
@@ -1,3 +1,7 @@
+#This script should be used after the correct phase profile of the 2nd lens 
+#is determined with the 'EDOF_WFOV_Bilayer_Paraxial_phaseProfileExtraction.py' script
+#The script characterizes the Point Spread Function as a point source propagates through the bi-layer to the imaging plane
+
 import csv, time
 import numpy as np
 import matplotlib.pyplot as plt
